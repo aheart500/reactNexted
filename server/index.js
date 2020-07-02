af7d12c53bfa009b96eab9ab7a532c4279a00211
MohamedNasser <aheart500@yahoo.com>
2020-07-02 06:36:47 +0200

    Image Src

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ mongoose
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/images/", express.static("../public/images"));
+/* app.use("/images/", express.static("../public/images")); */
 app.use("/api/admin", adminRouter);
 app.use("/api/user", userRouter);
 
